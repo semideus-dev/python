01b5320f3335cf9c859750aa55b968f408e8201b
Semideus <semideus.dev@gmail.com>
2024-12-21 22:26:12 +0530

    1. Introduction

--- 1_into.py
+++
@@ -1,9 +0,0 @@
-'''
-Python is a high level, general purpose programming language. 
-It is well regarded for its versatility, beginner friendly nature 
-and supporting multiple programming paradigms.
-
-Python can easily be installed from its official website.
-'''
-
-print("hello world!")
---
+++ 1_intro.py
@@ -0,0 +1,9 @@
+'''
+Python is a high level, general purpose programming language.
+It is well regarded for its versatility, beginner friendly nature
+and supporting multiple programming paradigms.
+
+Python can easily be installed from its official website.
+'''
+
+print("hello world!")
